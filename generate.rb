76b1381f0e2a7a12a26f12b6cb207f5ce2e1eacb
norisuke3 <norisuke3@a6de146f-eb04-a340-9df5-2e6c817b5a5a>
2009-01-11 09:28:27 +0000

    Added iKnowMyListManager.html generator.
    git-svn-id: https://firedictionary.svn.sourceforge.net/svnroot/firedictionary/trunk@240 a6de146f-eb04-a340-9df5-2e6c817b5a5a

---
+++ generate.rb
@@ -0,0 +1,21 @@
+array = []
+
+File.open("src/content/firedictionary/iknow/iKnowMyListManager-template.html", "r") {|file|
+  file.each { |line| 
+    if /<script-src src="(.*)"\/>/ =~ line then
+      file = "src/content/firedictionary/" + $1
+      File.open(file, "r"){|file2|
+        file2.each() {|line2| 
+          array.push line2.chomp("\r\n")
+        }
+      }
+    else
+      array.push line 
+    end
+  }
+}
+
+File.open("src/content/firedictionary/iknow/iKnowMyListManager.html", "w"){|f|
+  array.each {|line| f.puts(line)}
+}
+
